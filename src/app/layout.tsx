b1e84e1cd7bc0c3570da1d4b601f09e19ce5e2ff
Juanjbrtz <122833621+Juanjbrtz@users.noreply.github.com>
2026-08-24 11:19:42 -0500

    load account and admin visual system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
 import "./info.css";
+import "./account-admin.css";
 
 const instrumentSans = Instrument_Sans({
   variable: "--font-brand",
