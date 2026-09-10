a17cabb10986cfe00ad876f251288e3805922165
Juanjbrtz <122833621+Juanjbrtz@users.noreply.github.com>
2026-09-10 17:37:09 -0500

    Remove remote image URL support

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: "8mb",
