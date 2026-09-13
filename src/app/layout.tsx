7a1d6d6f01a48df3970ea2ae46bfb26c2797e4dd
Juanjbrtz <122833621+Juanjbrtz@users.noreply.github.com>
2026-09-13 00:37:30 -0500

    Aplicar premium flow v2 al preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ import "./catalog-refinement.css";
 import "./dark-refined-preview.css";
 import "./cart-drawer-refined.css";
 import "./cart-drawer-polish.css";
+import "./premium-flow-v2.css";
 
 const instrumentSans = Instrument_Sans({ variable: "--font-brand", subsets: ["latin"] });
 
